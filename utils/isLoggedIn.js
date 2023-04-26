85d15f82bb6dff7bfe5c29a613be7f5c80ab9cf4
OmarHazemElesawy <omarhazem2204@gmail.com>
2023-04-26 22:06:07 +0200

    added isloggedIn and returnTo middleware

--- utils/isLoggedIn.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    req.session.returnTo = req.originalUrl;
-    req.flash("error", "You must login first");
-    return res.redirect("/login");
-  }
-  next();
-};
