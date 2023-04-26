11720b4fc9092a8c0f3d0a656c2d36f5d10e69a4
OmarHazemElesawy <omarhazem2204@gmail.com>
2023-04-26 15:35:03 +0200

    added passport authentication login and setup

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const path = require("path");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const passportLocal = require("passport-local");
 
+//file imports
+const ExpressError = require("./utils/ExpressError");
 const campgroundsRoutes = require("./routes/campgrounds");
 const reviewsRoutes = require("./routes/reviews");
+const usersRoutes = require("./routes/users");
+const User = require("./models/user");
 
 //Express
 const app = express();
@@ -32,11 +38,21 @@ app.use(session(sessionConfig));
 //Flash
 app.use(flash());
 
-app.use((req,res,next) => {
+
+//Passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new passportLocal(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//local storage
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
-})
+});
 
 //method override
 app.use(methodOverride("_method"));
@@ -69,6 +85,7 @@ app.get("/", (_req, res) => {
 //Express Routers
 app.use("/campgrounds", campgroundsRoutes);
 app.use("/campgrounds/:id/reviews", reviewsRoutes);
+app.use("/", usersRoutes);
 
 //Catch all route, passes error to error handler
 app.all("*", (_req, _res, next) => {
