c1b889f20084e2bd28508df7369a288274c67e79
OmarHazemElesawy <omarhazem2204@gmail.com>
2023-04-27 12:47:07 +0200

    migrate server side validation for campgrounds and reviews

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,3 +1,8 @@
+const { campgroundSchema, reviewSchema } = require("../schemas");
+
+const ExpressError = require("../utils/ExpressError");
+
+//checking logging in
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
@@ -7,9 +12,33 @@ module.exports.isLoggedIn = (req, res, next) => {
   next();
 };
 
+//return to
 module.exports.storeReturnTo = (req, res, next) => {
   if (req.session.returnTo) {
-      res.locals.returnTo = req.session.returnTo;
+    res.locals.returnTo = req.session.returnTo;
   }
   next();
-}
+};
+
+//server side validate using JOI
+
+//campground validation
+module.exports.validateCampground = (req, _res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+//review validation
+module.exports.validateReview = (req, _res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
