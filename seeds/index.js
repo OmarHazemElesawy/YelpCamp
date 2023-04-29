e4970e04a1879601dbb5d86292efa55754cbb011
OmarHazemElesawy <omarhazem2204@gmail.com>
2023-04-29 15:19:45 +0300

    file cleanup and adding remote mongodb connection url

--- seeds/index.js
+++ seeds/index.js
@@ -1,21 +1,17 @@
 //self contained seeding file, run it to delete all data and seed it all over again
 //imports
+require("dotenv").config({ path: "../.env" });
 const mongoose = require("mongoose");
 const cities = require("./seeds");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 
-// //maps seeding
-// const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-// const mapboxToken =
-//   "pk.eyJ1IjoiZ2VvLW1hcHMtY291cnNlIiwiYSI6ImNsaDBjZTNxMjBzMjczZ3FjbzUyajh0bXcifQ.FI1oZN-SrZIC11ZKR0Bk2w";
-// const geocoder = mbxGeocoding({ accessToken: mapboxToken });
-
 //mongodb connection
+const dbUrl = process.env.DB_URL;
 mongoose.set("strictQuery", true);
 mongoose
-  .connect("mongodb://127.0.0.1:27017/yelpCamp", {
+  .connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -37,41 +33,31 @@ const seedDB = async (count) => {
     const location = `${randLocation.city}, ${randLocation.state}`;
     const geometry = { type: 'Point', coordinates: [randLocation.longitude, randLocation.latitude] };
     const newCampground = new Campground({
-      author: "644c03d8c53712220f026e09",
+      author: process.env.ADMIN_ID,
       geometry,
       location,
       title: `${randGen(descriptors)}, ${randGen(places)}`,
       description:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis distinctio rerum exercitationem architecto iusto eius magni maxime laboriosam ea? Minima, ipsa quidem nam perspiciatis modi tempora sapiente eos ipsam! Facilis.",
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis distinctio rerum exercitationem architecto iusto eius magni maxime laboriosam ea? Minima, ipsa quidem nam perspiciatis modi tempora sapiente eos ipsam! Facilis. Cupiditate ex debitis, voluptas nobis rem veritatis.",
       price: randPrice,
       images: [
         {
-          url: "https://res.cloudinary.com/dgglpas6e/image/upload/v1682602873/YelpCamp/lwglck0dfohphxnew1pq.jpg",
-          filename: "YelpCamp/lwglck0dfohphxnew1pq",
+          url: "https://res.cloudinary.com/dgglpas6e/image/upload/v1682760987/YelpCamp/mzqkkps9ii1t0zs3q8xt.jpg",
+          filename: "YelpCamp/mzqkkps9ii1t0zs3q8xt",
         },
         {
-          url: "https://res.cloudinary.com/dgglpas6e/image/upload/v1682602873/YelpCamp/b8guq0lob7m24cxlww4n.jpg",
-          filename: "YelpCamp/b8guq0lob7m24cxlww4n",
+          url: "https://res.cloudinary.com/dgglpas6e/image/upload/v1682760987/YelpCamp/zynftiygq4awonhugr0r.jpg",
+          filename: "YelpCamp/zynftiygq4awonhugr0r",
         },
         {
-          url: "https://res.cloudinary.com/dgglpas6e/image/upload/v1682602873/YelpCamp/klkrd76kurkz8qxr7pm5.jpg",
-          filename: "YelpCamp/klkrd76kurkz8qxr7pm5",
+          url: "https://res.cloudinary.com/dgglpas6e/image/upload/v1682760987/YelpCamp/pu3htksr386kfntvpg6l.jpg",
+          filename: "YelpCamp/pu3htksr386kfntvpg6l",
         },
       ],
     });
     await newCampground.save();
   }
 };
-
-// const genGeoCode = async (location) => {
-//   const geoData = await geocoder
-//     .forwardGeocode({
-//       query: location,
-//       limit: 1,
-//     })
-//     .send();
-//   return geoData.body.features[0].geometry;
-// };
 
 //200 entries
 seedDB(200).then(() => {
